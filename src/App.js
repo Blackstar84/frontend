5aeaccea2f5ac78cad7d4c95175143aebe60117f
Blackstar84 <carlosalmada84@gmail.com>
2023-04-17 20:08:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import Users from './user/pages/Users';
+
+// instalar Router
+// npm install --save react-router-dom@5 --save-exact 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      //Ruta que verificara si es / y no tiene nada 
+      <Route path="/" exact>
+        //Carga la página Users
+        <Users />
+      </Route>
+
+      //Luego de Recorrer todos los Route si no encuentra una ruta que pongamos 
+      // Redireccionará a un path específico
+      <Redirect to="/" ></Redirect>
+
+    </Router>
   );
 }
 
